df1fbd288699eeb62021a56168bcbe11f59c7f8d
jmcneil35 <49593714+jmcneil35@users.noreply.github.com>
2020-02-08 14:18:19 -0500

    Update example-smart-app.js

--- example-smart-app/src/js/example-smart-app.js
+++ example-smart-app/src/js/example-smart-app.js
@@ -48,7 +48,7 @@
           p.gender = gender;
           p.fname = fname;
           p.lname = lname;
-          p.height = getQuantityValueAndUnit(height[0]);
+          p.height = height;
           p.weight = getQuantityValueAndUnit(weight[0]);
 
           if (typeof systolicbp != 'undefined')  {
